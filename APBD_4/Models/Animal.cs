f5a48d140f0baec700c98f99fae3f30259e9b64c
FoggyDeer <paniotoffmichael@gmail.com>
2024-04-13 22:18:30 +0200

    Animal class created

--- APBD_4/Models/Animal.cs
+++ APBD_4/Models/Animal.cs
@@ -3,8 +3,8 @@ namespace APBD_4.Models;
 public class Animal
 {
     public int IdAnimal { get; set; }
-    public string Name { get; set; }
-    public string Category { get; set; }
+    public string? Name { get; set; }
+    public string? Category { get; set; }
     public double Mass { get; set; }
-    
+    public string? FurColor { get; set; }
 }
