98409c7f88dbbaaec423a9ef469a106175e36b52
FoggyDeer <paniotoffmichael@gmail.com>
2024-04-13 23:04:09 +0200

    Added 5 api methods (GetAnimals, GetAnimal, AddAnimal, UpdateAnimal, DeleteAnimal)

--- APBD_4/Program.cs
+++ APBD_4/Program.cs
@@ -1,3 +1,5 @@
+using APBD_4.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +18,53 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//
+ var _animals = new List<Animal>();
+
+app.MapGet("/api/animals", () => Results.Ok(_animals))
+    .WithName("GetAnimals")
+    .WithOpenApi();
+
+app.MapGet("/api/animals/{id:int}", (int id) =>
+    {
+        var animal = _animals.FirstOrDefault(a => a.IdAnimal == id);
+        return animal == null ? Results.NotFound($"Animals with id {id} was not found") : Results.Ok(animal);
+    })
+    .WithName("GetAnimal")
+    .WithOpenApi();
+
+app.MapPost("/api/animals", (Animal animal) =>
+    {
+        _animals.Add(animal);
+        return Results.StatusCode(StatusCodes.Status201Created);
+    })
+    .WithName("AddAnimal")
+    .WithOpenApi();
+
+app.MapPut("/api/animals/{id:int}", (int id, Animal animal) =>
+    {
+        var animalToEdit = _animals.FirstOrDefault(a => a.IdAnimal == id);
+        if (animalToEdit == null)
+        {
+            return Results.NotFound($"Student with id {id} was not found");
+        }
+        _animals.Remove(animalToEdit);
+        _animals.Add(animal);
+        return Results.NoContent();
+    })
+    .WithName("UpdateAnimal")
+    .WithOpenApi();
+
+app.MapDelete("/api/animals/{id:int}", (int id) =>
+    {
+        var animalToDelete = _animals.FirstOrDefault(s => s.IdAnimal == id);
+        if (animalToDelete == null)
+        {
+            return Results.NoContent();
+        }
+        _animals.Remove(animalToDelete);
+        return Results.NoContent();
+    })
+    .WithName("DeleteAnimal")
+    .WithOpenApi();
 
 app.Run();
