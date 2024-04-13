17ea70b81fed669f8929bbc882ea1fcaadfd92f8
FoggyDeer <paniotoffmichael@gmail.com>
2024-04-13 22:08:56 +0200

    Initial commit

---
+++ APBD_4/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace APBD_4.Models;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Mass { get; set; }
+    
+}
